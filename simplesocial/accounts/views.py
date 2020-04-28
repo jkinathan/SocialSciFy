39ce357cc5c369fc2c96dbdde3cffee289fadfc6
jkinobe <jkinobe@gmail.com>
2020-04-28 11:52:25 +0300

    fast setups

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from accounts.models import User
+from django.urls import reverse_lazy #if you are logged in or logged out where you shud actually go
+from . import forms
+from django.views.generic import CreateView
 # Create your views here.
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login')
