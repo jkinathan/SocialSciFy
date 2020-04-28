b1914ffcfe862773dd54a4a0b68dffbaa3542823
jkinobe <jkinobe@gmail.com>
2020-04-28 11:57:44 +0300

    accounts app setup compelete

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -7,4 +7,6 @@ from django.views.generic import CreateView
 
 class SignUp(CreateView):
     form_class = forms.UserCreateForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('login') #reverse lazy you dont want this to execute until they have hit that signup button otherwise normal reverse wil just redirect them to login directly without letting them first signup
+    template_name = 'accounts/signup.html'
+    
