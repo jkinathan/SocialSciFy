c63195678857128dae9477c6cdd9d63cd939ef29
jkinobe <jkinobe@gmail.com>
2020-05-01 12:42:20 +0300

    groups model1

--- simplesocial/groups/models.py
+++ simplesocial/groups/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+#groups models.py file
+import misaka
+
+from django.contrib.auth import get_user_model
+User = get_user_model() #get current user instance 
+
+from django import template
+register = template.Library()
+
+
+
