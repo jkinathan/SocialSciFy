f5c76e353b6400e3204c9745adc323b2e89dea0b
jkinobe <jkinobe@gmail.com>
2020-05-04 12:00:12 +0300

    posts model

--- simplesocial/posts/models.py
+++ simplesocial/posts/models.py
@@ -14,6 +14,23 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now=True)
     message = models.TextField()
     message_html = models.TextField(editable=False)
-    group = models.ForeignKey(Group)
+    group = models.ForeignKey(Group, related_name="posts", null=True, blank=True)
+    
+    def __str__(self):
+        return self.message
+    
+    def save(self, *args, **kwargs):
+        #self.message_html = misaka.html(self.message)
+        super().save(*args, **kwargs)
+        
+    def get_absolute_url(self):
+        return reverse("posts:single", kwargs={"username": self.user.username, 'pk':self.pk})
+    
+    class Meta:
+        ordering = ['-created_at'] #minus - is for DESC order
+        unique_together = ['user','message']
+    
+        
+    
     
 
