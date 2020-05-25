cd52b34559c892fa6efbdf12301d5bab0a6c8fc7
jkinobe <jkinobe@gmail.com>
2020-05-25 11:27:24 +0300

    post views1

--- simplesocial/posts/views.py
+++ simplesocial/posts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.urls import reverse_lazy
+from django.http import Http404
+from django.views import generic
+
+from braces.views import SelectRelatedMixin
+
+from . import forms
+from . import models
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 
 # Create your views here.
